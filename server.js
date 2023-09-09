2968b51b4f80d37abcd1f634ba14ff1c40c8bafc
“Paula <weigeyang1007@gmail.com>
2023-09-08 18:24:42 -0700

    change back port to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./routes.js');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
